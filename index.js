0f2158515cc868f09fcd7ab7fc09d8824858f708
Raveen K Sharma <raveen08@gmail.com>
2021-02-11 20:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function Scooter(year, color, model) {
     this.year = year;
     this.color = color;
-    this.model = function() {
-        console.log(`Hello everybody, my year is ${this.year} whom you've been
-mailing at ${this.color}!`);
-    };
+    this.model = model;
 }
