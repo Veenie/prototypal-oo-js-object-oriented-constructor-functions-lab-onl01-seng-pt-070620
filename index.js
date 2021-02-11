ad148d478fc4d0f9440c0f4906f7e9a7fec5e605
Raveen K Sharma <raveen08@gmail.com>
2021-02-11 20:16:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function Scooter(year, color, model) {
+    this.name = name;
+    this.email = email;
+    this.sayHello = function() {
+        console.log(`Hello everybody, my name is ${this.name} whom you've been
+mailing at ${this.email}!`);
+    };
+}
